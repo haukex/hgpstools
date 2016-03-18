588cb88eeac008e73247339bad4510679cf5bfcc
Hauke D <haukex@zero-g.net>
2016-03-18 11:43:53 +0100

    Stripped daemon code out of serlog.pl
    I plan to replace it by Daemon::Control later.

--- serlog.pl
+++ serlog.pl
@@ -3,28 +3,19 @@ use warnings;
 use strict;
 use 5.010; no feature 'switch';
 
-### BEGIN INIT INFO
-# Provides:          serialdaemon_gps
-# Required-Start:    $local_fs $time
-# Required-Stop:     $local_fs
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-### END INIT INFO
-
 =head1 SYNOPSIS
 
-This is a daemon that logs line-based data (CRLF or LF) from a serial port;
-the default version stores GPS NMEA data with an added timestamp.
+B<This is a work in progress.> Please use C<serialdaemon_gps.pl> for now.
+
+This is a logger that logs line-based data (CRLF or LF) from a serial port.
 It also handles USB devices being hot-plugged.
 
- serialdaemon_gps.pl start|stop|status
+ serlog.pl CONFIGFILE.pl
+
+B<Warning:> C<CONFIGFILE.pl> will be executed by this script.
+For security, only use absolute pathnames and scripts you trust.
 
 =head1 DETAILS
-
-Note that although I've had this script running reliably for a while,
-it's still a little rough around the edges in that configuration and
-customization requires editing the script itself (configuration options
-are variables set at the beginning of the script).
 
 The subroutine stored in C<$HANDLE_LINE> handles checking input format as
 well as manipulating the input lines (such as adding timestamps).
@@ -34,23 +25,9 @@ a false value, and then the current C<$HANDLE_LINE> implementation
 simply logs input lines with a timestamp without checking their format.
 
 The subroutine stored in C<$HANDLE_STATUS> handles the manipulation of
-status messages from the daemon such as "start", "stop", "connect" and "disconnect".
+status messages from the logger such as "start", "stop", "connect" and "disconnect".
 By default it outputs the messages with a timestamp (i.e. they are mixed
 in with the NMEA stream).
-
-This script contains an init script header, and it can be installed
-as a service directly, as follows (adjust paths as necessary).
-This script attempts to change its UID and GID as configured, which
-is useful when running as root.
-
- sudo ln -vs /home/user/serialdaemon_gps.pl /etc/init.d/serialdaemon_gps
- sudo update-rc.d serialdaemon_gps defaults
- sudo service serialdaemon_gps start
-
-(It can be removed via C<sudo update-rc.d -f serialdaemon_gps remove>.)
-For more information on init scripts see L<https://wiki.debian.org/LSBInitScripts> and L<insserv(8)>.
-Note this script does not fully implement the spec, such as the "restart"
-command or all of the exit codes.
 
 You may need to add the user to the group C<dialout> (in Debian, this
 normally gives full and direct access to serial ports):
@@ -78,6 +55,8 @@ and then your device will always be available as F</dev/usb_gps>.
 Hint: For debugging, you can simply C<cat /dev/ttyUSB0> after setting
 the serial port speed via C<stty -F /dev/ttyUSB0 4800 raw>.
 Also, you can use L<minicom(1)>: C<minicom -D/dev/ttyUSB0>
+(configure via C<Ctrl-A o>, exit via C<Ctrl-A q>)
+or L<screen(1)>: C<screen /dev/ttyUSB0 4800> (exit via C<Ctrl-A \>)
 
 =head1 AUTHOR, COPYRIGHT, AND LICENSE
 
@@ -102,22 +81,9 @@ along with this program. If not, see L<http://www.gnu.org/licenses/>.
 
 # ### Application-Specific User Settings ###
 
-my $DEBUG = 0;
-
-# the following $user just decides between my computer and my RPi,
-# you can do this however you like
-my $user = -e '/home/haukex' ? 'haukex' : 'pi';
-die "I'm not sure what box I'm running on, please check the configuration"
-	unless -e "/home/$user";
-my $WANT_UID = getpwnam($user) or die "failed to get UID";
-my $WANT_GID = getgrnam($user) or die "failed to get GID";
-my $WANT_GRP2 = getgrnam('dialout') or die "failed to get GID of dialout";
-my $WANT_UMASK = oct('0027');
+#TODO: move this to CONFIGFILE.pl (and into a hash)
 my $SERIALPORT = '/dev/usb_gps';
 my $BAUDRATE = 4800;
-my $PIDFILE = "/home/$user/serialdaemon/serialdaemon_gps.pid";
-my $OUTFILE = "/home/$user/serialdaemon/gps_out.txt";
-my $ERRFILE = "/home/$user/serialdaemon/gps_err.txt";
 my $MAX_ERRORS = 100;
 
 use Time::HiRes qw/ gettimeofday /;
@@ -163,61 +129,24 @@ my $HANDLE_STATUS = sub { # code should edit $_; return value ignored
 	return;
 };
 
-# ###
 
-# FIRST, set our PID/GID/umask
-set_uid_gid();
-umask $WANT_UMASK;
-die "Error: umask change failed" unless umask==$WANT_UMASK;
-
-# ### Daemon Stuff ###
 use Getopt::Std 'getopts';
 use Pod::Usage 'pod2usage';
-use FindBin;
-# we want to use our patched version of Daemon::Daemonize until a new version gets released
-# see also http://github.com/robertkrimen/Daemon-Daemonize/pull/2
-use lib "$FindBin::RealBin/Daemon-Daemonize/lib";
-use Daemon::Daemonize 0.0052_01 qw/ check_pidfile write_pidfile delete_pidfile daemonize /;
 
 sub HELP_MESSAGE { pod2usage(-output=>shift); return }
-sub VERSION_MESSAGE { say {shift} q$serialdaemon.pl v1.00$; return }
+sub VERSION_MESSAGE { say {shift} q$serlog.pl v1.00$; return }
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
 getopts('', \my %opts) or pod2usage;
 pod2usage unless @ARGV==1;
-my $STARTSTOP = $ARGV[0];
+my $CONFIGFILE = $ARGV[0];
+#TODO: execute $CONFIGFILE
 
-my $oldpid = check_pidfile($PIDFILE);
-if ($STARTSTOP eq 'stop') {
-	$oldpid or die "Error: Daemon is not running, can't stop\n";
-	if ( kill('TERM', $oldpid)==1 )
-		{ warn "Sent SIGTERM to PID $oldpid\n"; exit }
-	else
-		{ die "Error: Failed to signal PID $oldpid" }
-}
-elsif ($STARTSTOP eq 'start') {
-	$oldpid and die "Error: Daemon is already running as PID $oldpid\n";
-}
-elsif ($STARTSTOP eq 'status') {
-	warn $oldpid
-		? "Status: Daemon is running as PID $oldpid\n"
-		: "Status: Daemon is not running\n" ;
-	exit;
-}
-else { pod2usage }
-
-warn "Daemonizing...\n";
-daemonize(stdout=>$OUTFILE, stderr=>$ERRFILE, umask=>$WANT_UMASK);
 local $SIG{__WARN__} = sub { warn "[".scalar(gmtime)." UTC] (PID $$) ".shift };
 local $SIG{__DIE__} = sub { die "[".scalar(gmtime)." UTC] (PID $$) FATAL ".shift };
 my $run=1;
 local $SIG{INT} = sub { warn "Caught SIGINT, stopping...\n"; $run=0 };
 local $SIG{TERM} = sub { warn "Caught SIGTERM, stopping...\n"; $run=0 };
 local $|=1;
-write_pidfile($PIDFILE);
-my $PID_FILE_WRITTEN = 1;
-END { delete_pidfile($PIDFILE) if $PID_FILE_WRITTEN }
-die "Error: desired umask not in effect" unless umask==$WANT_UMASK;
-
 
 # ### Main Loop ###
 use Device::SerialPort 1.04 ();
@@ -299,28 +228,3 @@ sub error {
 		{ warn "Error: $msg; continuing ($errs errors)\n" }
 	return;
 }
-
-sub set_uid_gid {
-	# reduce permissions by changing user & group
-	# (in case this is run by root, which is likely under init)
-	warn "Debug: Before: RUID=$<, EUID=$>, RGID=$(, EGID=$), umask=".sprintf("%o",umask)."\n" if $DEBUG;
-	if ( $) !~ /^$WANT_GID\b.*\b$WANT_GRP2\b/ ) {
-		$) = "$WANT_GID $WANT_GRP2";  ## no critic (RequireLocalizedPunctuationVars)
-		die "Error: EGID change failed: $!" if $! || $) ne "$WANT_GID $WANT_GRP2";
-	}
-	if ( $( != $WANT_GID ) {
-		$( = $WANT_GID;  ## no critic (RequireLocalizedPunctuationVars)
-		die "Error: RGID change failed: $!" if $! || $( != $WANT_GID;
-	}
-	if ( $> != $WANT_UID ) {
-		$> = $WANT_UID;  ## no critic (RequireLocalizedPunctuationVars)
-		die "Error: EUID change failed: $!" if $! || $> != $WANT_UID;
-	}
-	if ( $< != $WANT_UID ) {
-		$< = $WANT_UID;  ## no critic (RequireLocalizedPunctuationVars)
-		die "Error: RUID change failed: $!" if $! || $< != $WANT_UID;
-	}
-	warn "Debug: After: RUID=$<, EUID=$>, RGID=$(, EGID=$), umask=".sprintf("%o",umask)."\n" if $DEBUG;
-	return;
-}
-
