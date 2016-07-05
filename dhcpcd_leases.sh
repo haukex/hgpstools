8cf33d14f621c92b4f2b0230db8f99d2fa88865b
Hauke D <haukex@zero-g.net>
2016-07-05 16:54:36 +0200

    Ignore loopback interface when getting leases
    This is a quick fix to prevent the syslog from filling up with messages
    like "dhcpcd[23530]: lo: no lease to dump"

--- dhcpcd_leases.sh
+++ dhcpcd_leases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # simple script to fetch lease information for all known interfaces
-for IF in $(ifconfig -a | perl -lne 'print $1 if /^(\S+)\s/')
+for IF in $(ifconfig -a | perl -lne 'print $1 if /^(\S+)\s/ && $1 ne "lo"')
 do
 	echo "[$IF]"
 	dhcpcd -U $IF
